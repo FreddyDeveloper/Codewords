21ac2fac923ec620308fb9a1edce070ec6a3b3ea
FreddyDeveloper <81062362+FreddyDeveloper@users.noreply.github.com>
2023-08-01 00:10:28 -0500

    Codewords_Searcher.py

--- Codewords Searcher.py
+++ Codewords Searcher.py
@@ -8,90 +8,90 @@ import re
 import logging
 from colorama import Fore, Style, init
 
-def buscar_texto_en_archivos(carpeta, palabras_buscar):
-    archivos_encontrados = []
-    tipos_archivo_buscar = ['.php', '.js', '.html', '.json', '.ini', '.css']
+def search_text_in_files(folder, search_words):
+    found_files = []
+    file_types_to_search = ['.php', '.js', '.html', '.json', '.ini', '.css']
 
-    # Escapar caracteres especiales en cada palabra buscada
-    palabras_buscar_regex = [re.escape(palabra) for palabra in palabras_buscar]
+    # Escape special characters in each search word
+    search_words_regex = [re.escape(word) for word in search_words]
 
-    # Crear una expresión regular que buscará todas las palabras
-    texto_buscar_regex = re.compile(r"|".join(palabras_buscar_regex), re.IGNORECASE)
+    # Create a regular expression that will search for all the words
+    search_text_regex = re.compile(r"|".join(search_words_regex), re.IGNORECASE)
 
-    # Configurar el sistema de logs con color
-    init(autoreset=True)  # Inicializar colorama
+    # Configure logging system with colors
+    init(autoreset=True)  # Initialize colorama
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger('buscador_log')
+    logger = logging.getLogger('search_log')
 
-    # Recorre todos los archivos en la carpeta y sus subcarpetas
-    for ruta_actual, carpetas, archivos in os.walk(carpeta):
-        # Excluye ciertas carpetas si es necesario
-        # Por ejemplo, si deseas excluir la carpeta 'excluir_carpeta':
-        # if 'excluir_carpeta' in carpetas:
-        #     carpetas.remove('excluir_carpeta')
+    # Traverse all files in the folder and its subfolders
+    for current_path, folders, files in os.walk(folder):
+        # Exclude certain folders if necessary
+        # For example, if you want to exclude the folder 'exclude_folder':
+        # if 'exclude_folder' in folders:
+        #     folders.remove('exclude_folder')
 
-        for archivo in archivos:
-            # Comprueba si el archivo tiene una extensión de archivo válida
-            if any(archivo.endswith(extension) for extension in tipos_archivo_buscar):
-                ruta_completa = os.path.join(ruta_actual, archivo)
+        for file in files:
+            # Check if the file has a valid file extension
+            if any(file.endswith(extension) for extension in file_types_to_search):
+                full_path = os.path.join(current_path, file)
 
                 try:
-                    # Lee el contenido del archivo
-                    with open(ruta_completa, 'r', encoding='utf-8') as archivo_contenido:
-                        contenido = archivo_contenido.read().lower()  # Convierte el contenido a minúsculas
+                    # Read the content of the file
+                    with open(full_path, 'r', encoding='utf-8') as file_content:
+                        content = file_content.read().lower()  # Convert content to lowercase
                         
-                        # Imprime las palabras que se están buscando en el archivo actual
-                        logger.info(f"{Fore.RED}Buscando palabras en el archivo: {ruta_completa} - Palabras: {', '.join(palabras_buscar)}")
+                        # Print the words being searched in the current file
+                        logger.info(f"{Fore.RED}Searching words in file: {full_path} - Word: {', '.join(search_words)}")
 
-                        # Busca todas las palabras en el contenido utilizando la expresión regular
-                        resultados = texto_buscar_regex.finditer(contenido)
-                        for resultado in resultados:
-                            archivos_encontrados.append((ruta_completa, resultado.group()))
+                        # Search all words in the content using the regular expression
+                        results = search_text_regex.finditer(content)
+                        for result in results:
+                            found_files.append((full_path, result.group()))
 
-                            # Imprime el resultado con la palabra encontrada en amarillo
-                            resultado_coloreado = re.sub(resultado.group(), f"{Fore.YELLOW}{resultado.group()}{Fore.WHITE}", resultado.group())
-                            logger.info(f"{Fore.WHITE}{Style.BRIGHT}Texto encontrado en el archivo: {ruta_completa} - Palabra encontrada: {resultado_coloreado}")
+                            # Print the result with the found word in yellow
+                            colored_result = re.sub(result.group(), f"{Fore.YELLOW}{result.group()}{Fore.WHITE}", result.group())
+                            logger.info(f"{Fore.WHITE}{Style.BRIGHT}Text found in file: {full_path} - Found word: {colored_result}")
                 except Exception as e:
-                    logger.error(f"{Fore.RED}Error al abrir el archivo {ruta_completa}: {e}")
+                    logger.error(f"{Fore.RED}Error while opening the file {full_path}: {e}")
 
-    return archivos_encontrados
+    return found_files
 
-def obtener_palabras_buscadas():
-    # Obtener las palabras a buscar utilizando un cuadro de diálogo
-    palabras_buscadas = simpledialog.askstring("Codewords v1.2 by @FreddyDeveloper", "Ingrese las palabras que desea buscar en el código separadas por comas:")
+def get_search_words():
+    # Get the words to search using a dialog box
+    search_words = simpledialog.askstring("Codewords v1.2 by @FreddyDeveloper", "Enter the words you want to search in the code separated by commas:")
 
-    if not palabras_buscadas:
-        messagebox.showwarning("Advertencia", "No se ingresaron palabras de búsqueda.")
+    if not search_words:
+        messagebox.showwarning("Warning", "No search words were entered.")
         return None
 
-    # Convertir las palabras ingresadas en una lista
-    palabras_buscadas = [palabra.strip() for palabra in palabras_buscadas.split(",")]
-    return palabras_buscadas
+    # Convert the entered words into a list
+    search_words = [word.strip() for word in search_words.split(",")]
+    return search_words
 
-# Crear una ventana de Tkinter para que el usuario seleccione la carpeta de búsqueda
+# Create a Tkinter window for the user to select the search folder
 app = tk.Tk()
-app.withdraw()  # Oculta la ventana principal
+app.withdraw()  # Hide the main window
 
-# Obtener las palabras a buscar
-palabras_buscadas = obtener_palabras_buscadas()
+# Get the words to search
+search_words = get_search_words()
 
-if palabras_buscadas is None:
-    messagebox.showwarning("Advertencia", "No se ingresaron palabras de búsqueda. Saliendo...")
+if search_words is None:
+    messagebox.showwarning("Warning", "Exiting...")
 else:
-    # Seleccionar la carpeta de búsqueda
-    carpeta_seleccionada = filedialog.askdirectory(title="Selecciona una carpeta para iniciar la búsqueda:")
+    # Select the search folder
+    selected_folder = filedialog.askdirectory(title="Select a folder to start the search:")
 
-    if not carpeta_seleccionada:
-        messagebox.showwarning("Advertencia", "No se ha seleccionado ninguna carpeta. Saliendo...")
+    if not selected_folder:
+        messagebox.showwarning("Warning", "No folder has been selected. Exiting...")
     else:
-        # Llamar a la función para buscar las palabras en los archivos
-        archivos_encontrados = buscar_texto_en_archivos(carpeta_seleccionada, palabras_buscadas)
+        # Call the function to search for words in the files
+        found_files = search_text_in_files(selected_folder, search_words)
 
-        # Imprime los resultados en colores y formato personalizados
-        if archivos_encontrados:
-            print(f"{Fore.GREEN}{Style.BRIGHT}Texto encontrado en los siguientes archivos:")
-            for ruta_completa, palabra_encontrada in archivos_encontrados:
-                print(f"{Fore.GREEN}{Style.BRIGHT}{ruta_completa}")
-                print(f"{Fore.YELLOW}{Style.BRIGHT}Palabra encontrada: {palabra_encontrada}{Fore.WHITE}")
+        # Print the results with custom colors and formatting
+        if found_files:
+            print(f"{Fore.GREEN}{Style.BRIGHT}Text found in the following files:")
+            for full_path, found_word in found_files:
+                print(f"{Fore.GREEN}{Style.BRIGHT}{full_path}")
+                print(f"{Fore.YELLOW}{Style.BRIGHT}Found word: {found_word}{Fore.WHITE}")
         else:
-            print(f"{Fore.RED}{Style.BRIGHT}Texto no encontrado en ningún archivo.")
+            print(f"{Fore.RED}{Style.BRIGHT}Text not found in any file.")
